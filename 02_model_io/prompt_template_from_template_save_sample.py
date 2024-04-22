94c9ab10ac2a2745bb71ec2451c972d00a17f0c2
Matsui Masashi <5251092+circled9@users.noreply.github.com>
2024-04-22 22:21:00 +0900

    chapter 2, page 64

---
+++ 02_model_io/prompt_template_from_template_save_sample.py
@@ -0,0 +1,6 @@
+from langchain.prompts import PromptTemplate
+
+prompt = PromptTemplate(
+    template="{product}はどこの会社が開発した製品ですか？", input_variables=["product"]
+)
+prompt_json = prompt.save("prompt.json")
