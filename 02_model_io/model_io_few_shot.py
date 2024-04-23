efeee5149458c38496a38147b6a45aa214006c75
Matsui Masashi <5251092+circled9@users.noreply.github.com>
2024-04-23 22:20:27 +0900

    chapter 2, page 78

---
+++ 02_model_io/model_io_few_shot.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai.llms import OpenAI
+from langchain.prompts import FewShotPromptTemplate, PromptTemplate
+
+load_dotenv("../.env")
+API_KEY = os.getenv("OPENAI_API_KEY")
+
+examples = [
+    {
+        "input": "LangChainはChatGPT・Large Language Model (LLM)の実利用をより柔軟に簡易に行うためのツール群です",
+        "output": "LangChainは、ChatGPT・Large Language Model (LLM)の実利用をより柔軟に、簡易に行うためのツール群です。",
+    }
+]
+
+prompt = PromptTemplate(
+    input_variables=["input", "output"],
+    template="入力: {input}\n出力: {output}",
+)
+
+few_shot_prompt = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=prompt,
+    prefix="以下の句読点の抜けた入力に句読点を追加してください。追加して良い句読点は「、」「。」のみです。他の句読点は追加しないでください。",
+    suffix="入力: {input_string}\n出力:",
+    input_variables=["input_string"],
+)
+
+llm = OpenAI(
+    api_key=API_KEY,
+    model="gpt-3.5-turbo-instruct",
+)
+formatted_prompt = few_shot_prompt.format(
+    input_string="私はさまざまな機能がモジュールとして提供されているLangChainを使ってアプリケーションを開発しています"
+)
+result = llm.invoke(formatted_prompt)
+print("formatted_prompt: ", formatted_prompt)
+print("result: ", result)
